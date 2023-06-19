d85d2329944f0c774c86ff6856392b0e19975658
Sam Marxz <sammarxzz@gmail.com>
2023-06-19 13:37:03 +0000

    chore: add and configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -5,8 +8,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: '@', replacement: '/src' }
-    ]
-  }
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
 });
